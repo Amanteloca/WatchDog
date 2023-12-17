ecb2642b96f5b38100aae2310a0ee3d39eb8b102
amanteloca <tonyanaky51@gmail.com>
2023-12-17 07:07:04 +0000

    added profile route

--- app.py
+++ app.py
@@ -159,3 +159,15 @@ def register():
     # Render the registration form template for both GET and POST requests
     return render_template('register.html', msg=msg)
 
+@app.route('/pythonlogin/profile')
+def profile():
+    # Check if the user is logged in
+    if 'loggedin' in session:
+        # We need all the account info for the user so we can display it on the profile page
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
+        account = cursor.fetchone()
+        # Show the profile page with account info
+        return render_template('profile.html', account=account)
+    # User is not logged in redirect to login page
+    return redirect(url_for('login'))
